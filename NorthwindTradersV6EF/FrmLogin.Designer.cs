e11b2d700982339c33d33fe385f5a3328b1af475
moralesrmarco <marco2003_6@hotmail.com>
2026-03-26 14:33:15 -0600

    se modifico el frmlogin.designer para que aparezca el usuario y la contraseña al inicial el sistema

--- NorthwindTradersV6EF/FrmLogin.Designer.cs
+++ NorthwindTradersV6EF/FrmLogin.Designer.cs
@@ -42,9 +42,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(142, 108);
+            this.label1.Location = new System.Drawing.Point(106, 88);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.Size = new System.Drawing.Size(65, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario:";
             // 
@@ -52,38 +53,44 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(111, 163);
+            this.label2.Location = new System.Drawing.Point(83, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 20);
+            this.label2.Size = new System.Drawing.Size(90, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña:";
             // 
             // txtUsuario
             // 
             this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtUsuario.Location = new System.Drawing.Point(232, 105);
+            this.txtUsuario.Location = new System.Drawing.Point(174, 85);
+            this.txtUsuario.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUsuario.MaxLength = 20;
             this.txtUsuario.Name = "txtUsuario";
-            this.txtUsuario.Size = new System.Drawing.Size(190, 26);
+            this.txtUsuario.Size = new System.Drawing.Size(144, 22);
             this.txtUsuario.TabIndex = 2;
+            this.txtUsuario.Text = "mmorales";
             // 
             // txtPwd
             // 
             this.txtPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPwd.Location = new System.Drawing.Point(232, 160);
+            this.txtPwd.Location = new System.Drawing.Point(174, 130);
+            this.txtPwd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPwd.MaxLength = 20;
             this.txtPwd.Name = "txtPwd";
-            this.txtPwd.Size = new System.Drawing.Size(190, 26);
+            this.txtPwd.Size = new System.Drawing.Size(144, 22);
             this.txtPwd.TabIndex = 3;
+            this.txtPwd.Text = "123456";
             this.txtPwd.UseSystemPasswordChar = true;
             // 
             // btnEntrar
             // 
             this.btnEntrar.AutoSize = true;
             this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEntrar.Location = new System.Drawing.Point(324, 217);
+            this.btnEntrar.Location = new System.Drawing.Point(243, 176);
+            this.btnEntrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEntrar.Name = "btnEntrar";
-            this.btnEntrar.Size = new System.Drawing.Size(98, 26);
+            this.btnEntrar.Size = new System.Drawing.Size(87, 23);
             this.btnEntrar.TabIndex = 4;
             this.btnEntrar.Text = "»» Entrar ««";
             this.btnEntrar.UseVisualStyleBackColor = true;
@@ -93,10 +100,10 @@
             // 
             this.btnTogglePwd.AutoSize = true;
             this.btnTogglePwd.Image = global::NorthwindTradersV6EF.Properties.Resources.mostrarCh;
-            this.btnTogglePwd.Location = new System.Drawing.Point(441, 155);
+            this.btnTogglePwd.Location = new System.Drawing.Point(331, 126);
             this.btnTogglePwd.Margin = new System.Windows.Forms.Padding(0);
             this.btnTogglePwd.Name = "btnTogglePwd";
-            this.btnTogglePwd.Size = new System.Drawing.Size(47, 37);
+            this.btnTogglePwd.Size = new System.Drawing.Size(35, 30);
             this.btnTogglePwd.TabIndex = 5;
             this.toolTip1.SetToolTip(this.btnTogglePwd, "Mostrar/Ocultar contraseña");
             this.btnTogglePwd.UseVisualStyleBackColor = true;
@@ -104,9 +111,9 @@
             // 
             // FrmLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(594, 346);
+            this.ClientSize = new System.Drawing.Size(446, 281);
             this.Controls.Add(this.btnTogglePwd);
             this.Controls.Add(this.btnEntrar);
             this.Controls.Add(this.txtPwd);
@@ -114,6 +121,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmLogin";
